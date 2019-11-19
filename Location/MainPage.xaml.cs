736a3ff18eee04d737ada80feca807d8e0de5641
chanchalpareek <chanchalpareek2511@gmail.com>
2019-11-19 17:04:03 -0500

    * .gitignore: initial commit
    * Location.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * Info.plist:
    * Location.csproj:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * Entitlements.plist:
    * MainActivity.cs:
    * Location.iOS.csproj:
    * Default.png:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * Location.Android.csproj:
    * Default-568h@2x.png:
    * Default-Portrait.png:
    * AssemblyInfo.cs:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * Tabbar.axml:
    * LaunchScreen.storyboard:
    * Toolbar.axml:
    * Default-Portrait@2x.png:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon87.png:
    * Icon20.png:
    * Icon60.png:
    * Icon58.png:
    * Icon40.png:
    * Icon29.png:
    * Icon76.png:
    * Icon80.png:
    * Icon152.png:
    * Icon180.png:
    * Icon167.png:
    * Icon120.png:
    * icon_round.xml:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ Location/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Xamarin.Forms;
+using Xamarin.Essentials;
+using System;
+
+namespace XamarinEssentials
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+        async void btnLocation_Clicked(object sender, System.EventArgs e)
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+
+                if (location != null)
+                {
+                    lblLatitude.Text = "Latitude: " + location.Latitude.ToString();
+                    lblLongitude.Text = "Longitude:" + location.Longitude.ToString();
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Faild", fnsEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Faild", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Faild", ex.Message, "OK");
+            }
+
+        }
+    }
+}
